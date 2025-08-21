58b86739cf4bdf570d4c4c8ec48cd17fa8df1e28
MDevHub <musbaudeenabdulrahman040@gmail.com>
2025-08-21 10:44:46 +0100

    Fixed the mobile responsiveness

--- src/app/shared/pages/home/page.tsx
+++ src/app/shared/pages/home/page.tsx
@@ -6,8 +6,8 @@ import ChatButton from "@/app/shared/components/chatButton/ChatButton"
 
 const HomePage = () => {
    return (
-      <div className="w-full pt-[160px] ">
-         <div className="max-w-7xl mx-auto px-6 md:px-12 lg:px-14 space-y-6">
+      <div className="w-full pt-[85px] sm:pt-[160px] ">
+         <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-12 lg:px-14 space-y-6">
             <OverviewLayout />
 
             <Trending />
